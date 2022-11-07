d4b22b6730717dfba60b0615f37332c2e6f1eb3b
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-11-07 18:35:18 +0100

    start building mutations.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,38 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token //! this requires token setup in App.js
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($book: Book!) {
+    saveBook(book: $book) { //! not sure this will work
+        bookId
+        authors//! not sure this is correct for handling array
+        description
+        title
+        image
+        link
+    }
+  }
+`;
