797fb5223d8c19eea711c588f4b18e4a577b4a4c
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-11-07 14:23:40 +0100

    define query and mutation types

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+type Query {
+  me: User //! do I need the square brackets or is that only used for arrays?
+  }
+
+type Mutation {
+  login(email: String!, password: String!): Auth
+
+  addUser(username: String!, email: String!, password: String!): Auth
+
+  saveBook(description: String!, title: String!, bookId: ID!, image: String!, link: String! ): User
+
+  removeBook(bookId: ID!): User
+}
+
+type User {
+  _id: ID
+  username: String
+  email: String
+  bookCount: Number
+  savedBooks: [Book]
+}
+
+type Book {
+  bookId: String
+  authors: [bookSchema] //! not sure this is right here; search "savedBooks: [bookSchema]" as example
+  description: String
+  title: String
+  image: String
+  link: String
+}
+
+type Auth {
+  token: ID!
+  user: User
+}
+`;
