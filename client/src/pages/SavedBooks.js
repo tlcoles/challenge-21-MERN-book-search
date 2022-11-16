ed4f63a7998a76ffa39639800151d86bc1f671a9
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-11-16 15:29:35 +0100

    remove old comments

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,57 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
 //! import useQuery, GET_ME, and useParams to use useQuery() hook
 import { useQuery, useMutation } from '@apollo/client';
-import { useParams } from 'react-router-dom';
 import { GET_ME } from '../utils/queries';
 
-
-import { getMe, deleteBook } from '../utils/API';
+import { deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
-//! use the useQuery() Hook to execute the GET_ME query on load and save it to a variable named userData
-//! Remove the useEffect() Hook that sets the state for UserData.
+// use the useQuery() Hook to execute the GET_ME query on load and save it to a variable named userData
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+  const [setUserData] = useState({});
 
-  const { _id } = useParams();
-  const { loading, data } = useQuery(GET_ME, {
-    variables: { userData: _id }
-  });
-
-  const me = data?.me || {}; //! not sure about this; have tested [] and {} with no difference but believe it may be important to retrieving books
-
-  // useEffect(() => {
-  //   const getUserData = async () => {
-  //     try {
-  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //       if (!token) {
-  //         return false;
-  //       }
-
-  //       const response = await getMe(token);
-
-  //       if (!response.ok) {
-  //         throw new Error('something went wrong!');
-  //       }
-
-  //       const user = await response.json();
-  //       setUserData(user);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-
-  //! not sure I do not need this for something since I cannot see saved books
-  //   getUserData();
-  // }, [userDataLength]);
-
+  const { loading, data } = useQuery(GET_ME);
+  const userData = data?.me || {}; 
 
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
@@ -66,7 +30,7 @@ const SavedBooks = () => {
       const response = await deleteBook(bookId, token);
 
       if (!response.ok) {
-        throw new Error('something went wrong!');
+        throw new Error("something went wrong!");
       }
 
       const updatedUser = await response.json();
@@ -79,13 +43,13 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
   return (
     <>
-      <Jumbotron fluid className='text-light bg-dark'>
+      <Jumbotron fluid className="text-light bg-dark">
         <Container>
           <h1>Viewing saved books!</h1>
         </Container>
@@ -93,19 +57,30 @@ const SavedBooks = () => {
       <Container>
         <h2>
           {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
+            ? `Viewing ${userData.savedBooks.length} saved ${
+                userData.savedBooks.length === 1 ? "book" : "books"
+              }:`
+            : "You have no saved books!"}
         </h2>
         <CardColumns>
           {userData.savedBooks.map((book) => {
             return (
-              <Card key={book.bookId} border='dark'>
-                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+              <Card key={book.bookId} border="dark">
+                {book.image ? (
+                  <Card.Img
+                    src={book.image}
+                    alt={`The cover for ${book.title}`}
+                    variant="top"
+                  />
+                ) : null}
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
-                  <p className='small'>Authors: {book.authors}</p>
+                  <p className="small">Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
-                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                  <Button
+                    className="btn-block btn-danger"
+                    onClick={() => handleDeleteBook(book.bookId)}
+                  >
                     Delete this Book!
                   </Button>
                 </Card.Body>
